b50bb0c37eaafc0bbb9437388aac4cd13f4e97a8
Eber Diniz <eber@dee.ufc.br>
2025-09-17 00:42:30 -0300

    gunicorn

--- BombaSerra/settings.py
+++ BombaSerra/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1t&864i*xvvw2ghrgwq)o^qnco1wcin&^kf!e!$+xdi@5k95#+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
