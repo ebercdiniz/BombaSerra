71d115e22f3bf9ce4994b244dbda717327677397
Eber Diniz <eber@dee.ufc.br>
2025-09-22 00:13:27 -0300

    checagem se o sistema esta online ou offline

--- BombaSerra/settings.py
+++ BombaSerra/settings.py
@@ -71,9 +71,8 @@ TEMPLATES = [
     },
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-SESSION_COOKIE_NAME = "sessionid_bombaserra"
 
 #STATICFILES_DIRS = [
 #    BASE_DIR / "static",  # para arquivos estáticos globais
--- Bombas/models.py
+++ Bombas/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 
 class Mode(models.Model):
     MODES = [
@@ -9,8 +8,6 @@ class Mode(models.Model):
         ("eber", "Bomba do Eber/Abel"),
     ]
     mode = models.CharField(max_length=20, choices=MODES, default="none")
-    last_ping = models.DateTimeField(default=timezone.now)
-
     def __str__(self):
         return self.get_mode_display()
 # Create your models here.
